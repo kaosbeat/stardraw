0ee1b33a03371526d6f60a2024c523b4ae49bcce
Kasper Jordaens <kasper.jordaens@gmail.com>
2022-09-27 16:21:37 +0200

    twee

--- data_intersect/faarpages.py
+++ data_intersect/faarpages.py
@@ -107,6 +107,6 @@ def invertedCat(word, scale, spacing, xmov):
         tweet.convertSVGtoTweet(s.svgfile, "prompts context, inverted " + word)
     state = "done"
 
-invertedCat("psychedelic", 4, 8, 15 )
+invertedCat("data", 4, 8, 15 )
 
 # print(pq.wordContext("cat"))
